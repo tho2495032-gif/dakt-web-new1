40f8b6e63d78178dc176057cf74ff061654078b7
tho2495032-gif <tho2495032@gmail.com>
2025-11-21 22:07:04 +0700

    script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ sensorsRef.on('value', (snapshot) => {
          
          // --- PHẦN ĐÃ SỬA: DÙNG 'muaroi' THAY VÌ 'cbmua' ---
          if (currentIcon && currentValue && data.muaroi !== undefined) {
-             if (data.muaroi == "1") {
+             if (data.muaroi == "0") {
                  currentIcon.textContent = "🌧️";
                  currentValue.textContent = "Đang mưa";
              } else {
@@ -317,3 +317,4 @@ function addLog(message, type) {
         danhSachLog.removeChild(danhSachLog.lastChild);
     }
 }
+
